c69c56727a8ace6ba3a6314237dd0fad951c2b9e
ZAmasu9245 <51511391+ZAmasu9245@users.noreply.github.com>
2019-07-16 10:23:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ client.on('ready', () => {
 client.on('message', message => {
     if(message.content === prefix + "mp pub") {
         message.delete()
-        message.guild.members.map(m => m.send("**Yo nous c’est la β2L**\n\n__Nous sommes un clan née très récemment et nous cherchons.__\n\n```- 10 Très bon builders\n\n - 2-3 modo\n\n - 1 manager\n\n - une communauté active\n\n- plusieurs graphistes très compétents\n\n- 2-3 Animateurs/trice ```\n\n **Shin :**\ **__N’hésitez pas à rejoindre leurs serveur est très sympas et travaillé !__**"))
+        message.guild.members.map(m => m.send("**Yo nous c’est la β2L**\n\n__Nous sommes un clan née très récemment et nous cherchons.__\n\n```- 10 Très bon builders\n\n - 2-3 modo\n\n - 1 manager\n\n - une communauté active\n\n- plusieurs graphistes très compétents\n\n- 2-3 Animateurs/trice ```\n\n **Shin :**\ **__N’hésitez pas à rejoindre leurs serveur est très sympas et travaillé !__** \n https://discord.gg/8ARUWnt"))
     }
 })
